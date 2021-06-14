a56f8013519342aa1cabd84b988ee459c24fbf2c
robinjoon <robin980108@naver.com>
2021-06-14 10:35:20 +0900

    Add javadoc
    Add javadoc

--- AccountBook/src/main/java/dao/AuthDao.java
+++ AccountBook/src/main/java/dao/AuthDao.java
@@ -5,17 +5,25 @@ import java.sql.SQLException;
 import java.util.List;
 
 import org.apache.tomcat.jdbc.pool.DataSource;
+import org.springframework.dao.DataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
-
+/**
+ * AuthDao is a Data Access Object that performs user authentication.
+ * @version 1.0
+ * @author robinjoon */
 public class AuthDao {
 	private JdbcTemplate jdbcTemplate;
 	
 	public AuthDao(DataSource dataSource) {
 		this.jdbcTemplate = new JdbcTemplate(dataSource);
 	}
-	
-	public boolean checkToken(String token) {
+	/**
+	 * @param token	A token for user authentication included in the HTTP header. 
+	 * @return Returns true if it matches the token stored in DB, otherwise false.
+	 * @throws DataAccessException - If there is no auth table in the database or the structure is different.  
+	 * */
+	public boolean checkToken(String token) throws DataAccessException{
 		String sql = "select COUNT(*) from auth where token = ?";
 		List<Integer> result = jdbcTemplate.query(sql, new RowMapper<Integer>() {
 
